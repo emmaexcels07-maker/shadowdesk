1a0f5a638079a9f5d46c6e914cd5718febc3fec8
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2025-12-16 18:08:30 +0100

    Describe the changes made

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,13 +1,13 @@
 import PrismaClientPkg from "@prisma/client";
 
-const { PrismaClient } = PrismaClientPkg;
+const PrismaClient = (PrismaClientPkg as any).PrismaClient || PrismaClientPkg.default || PrismaClientPkg;
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const globalForPrisma = global as unknown as { prisma?: typeof PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log: ["query"], // optional
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
